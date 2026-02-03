dd8f728dba2f47057ce5604950a7802ec2e24a5f
qiang <qiangpan@amd.com>
2026-02-03 22:16:50 +0800

    refine

--- run.sh
+++ run.sh
@@ -3,5 +3,6 @@ source .venv/bin/activate
 
 export TRITON_BUILD_WITH_CLANG_LLD=true
 export TRITON_BUILD_WITH_CCACHE=true
+apt install -y clang lld
+uv pip install build
 uv pip install -r python/requirements.txt
-uv pip install -e . --no-build-isolation
